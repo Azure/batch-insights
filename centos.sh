2b6173bc731ac6758d2ca2edce78ae20b33434ce
Timothee Guerin <tiguerin@microsoft.com>
2017-11-20 13:41:55 -0800

    Missing files

---
+++ centos.sh
@@ -0,0 +1,12 @@
+set -e
+yum -y install epel-release
+yum -y install gcc python-pip python-devel
+
+echo "Python version:"
+python --version
+echo "Pip version:"
+pip --version
+pip install psutil python-dateutil applicationinsights
+
+wget --no-cache https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py
+python nodestats.py > node-stats.log 2>&1 &
