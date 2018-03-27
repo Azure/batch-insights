5a79830e7d5e57b03fae0ae669e24aa3260ad983
Timothee Guerin <timothee.guerin@outlook.com>
2018-03-27 09:02:25 -0700

    Fixed python path (#9)

--- windows.ps1
+++ windows.ps1
@@ -20,7 +20,7 @@ if($exists)
 }
 
 Write-Host "Starting App insights background process in $env:AZ_BATCH_TASK_WORKING_DIR"
-$action = New-ScheduledTaskAction -WorkingDirectory $env:AZ_BATCH_TASK_WORKING_DIR -Execute 'Powershell.exe' -Argument "Start-Process python -ArgumentList ('.\nodestats.py','$env:AZ_BATCH_POOL_ID', '$env:AZ_BATCH_NODE_ID', '$env:APP_INSIGHTS_INSTRUMENTATION_KEY')  -RedirectStandardOutput .\node-stats.log -RedirectStandardError .\node-stats.err.log -NoNewWindow"  
+$action = New-ScheduledTaskAction -WorkingDirectory $env:AZ_BATCH_TASK_WORKING_DIR -Execute 'Powershell.exe' -Argument "Start-Process C:\Python36\python.exe -ArgumentList ('.\nodestats.py','$env:AZ_BATCH_POOL_ID', '$env:AZ_BATCH_NODE_ID', '$env:APP_INSIGHTS_INSTRUMENTATION_KEY')  -RedirectStandardOutput .\node-stats.log -RedirectStandardError .\node-stats.err.log -NoNewWindow"  
 $principal = New-ScheduledTaskPrincipal -UserID 'NT AUTHORITY\SYSTEM' -LogonType ServiceAccount -RunLevel Highest ; 
 Register-ScheduledTask -Action $action -Principal $principal -TaskName "batchappinsights" -Force ; 
 Start-ScheduledTask -TaskName "batchappinsights"; 
