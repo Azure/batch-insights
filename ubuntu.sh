a7db4d5ec9576401db8cc5e2d4b9fb314a23706a
Timothee Guerin <tiguerin@microsoft.com>
2018-05-22 15:05:22 -0700

    Revert comment

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,7 +3,6 @@ set -e
 apt-get update
 apt-get -y install python-dev python-pip
 pip install psutil python-dateutil applicationinsights
-# TODO-TIM revert branch
 wget --no-cache https://raw.githubusercontent.com/Azure/batch-insights/master/nodestats.py
 python --version
 python nodestats.py > node-stats.log 2>&1 &
