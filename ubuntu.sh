c595f17d1203720bd4a0fc55ff1e2ed47a85c599
Timothee Guerin <tiguerin@microsoft.com>
2018-05-22 11:07:58 -0700

    remove ubuntu

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,6 +3,6 @@ set -e
 apt-get update
 apt-get -y install python-dev python-pip
 pip install psutil python-dateutil applicationinsights
-wget --no-cache https://raw.githubusercontent.com/Azure/batch-insights/master/nodestats.py
+wget --no-cache https://raw.githubusercontent.com/Azure/batch-insights/feature/disk-usage/nodestats.py
 python --version
 python nodestats.py > node-stats.log 2>&1 &
