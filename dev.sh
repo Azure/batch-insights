c2ef94f8d8a3fa80ec87cf1b77d0b05d68767763
Timothee Guerin <tiguerin@microsoft.com>
2018-09-10 12:04:45 -0700

    WIp

--- dev.sh
+++ dev.sh
@@ -3,11 +3,11 @@ set -e
 apt-get update  
 apt-get install -y git binutils bison build-essential
 
-rm -rf /usr/local
+export GOROOT=/usr/local/go
+if [ -d "$GOROOT" ]; then rm -Rf $GOROOT; fi
 wget https://dl.google.com/go/go1.11.linux-amd64.tar.gz
 tar -xvf go1.11.linux-amd64.tar.gz
 mv go /usr/local
-export GOROOT=/usr/local/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
 echo GO version $(go version)
