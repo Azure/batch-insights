26e416d0321e1997bfdbb98287448e9b6e0f0ad2
Timothee Guerin <tiguerin@microsoft.com>
2018-09-10 10:37:10 -0700

    WIp

--- dev.sh
+++ dev.sh
@@ -1,9 +1,10 @@
 set -e
 
 apt-get update  
-apt-get install -y golang-go git
+apt-get install -y golang-1.11-go git
 
 git clone https://github.com/Azure/batch-insights -b feature/go
 
+echo GO version $(go --version)
 cd batch-insights
 go run ./..
