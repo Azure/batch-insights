ea2728216a3f6ce2bd4a95b25f543d132d3e8ba4
Timothee Guerin <tiguerin@microsoft.com>
2018-09-10 09:54:58 -0700

    Read stats

--- cmd/main.go
+++ cmd/main.go
@@ -11,16 +11,20 @@ import (
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/net"
 )
 
 type NodeStats struct {
-	memoryTotal uint64
-	memoryFree  uint64
+	memory      *mem.VirtualMemoryStat
 	cpuPercents []float64
 	diskUsage   []*disk.UsageStat
+	diskIO      *IOStats
+	netIO       *IOStats
 }
 
 var IS_PLATFORM_WINDOWS = runtime.GOOS == "windows"
+
+const STATS_POLL_RATE = time.Duration(5) * time.Second
 
 func pathExists(path string) (bool, error) {
 	_, err := os.Stat(path)
@@ -50,20 +54,58 @@ func getDiskToWatch() []string {
 
 func main() {
 	printSystemInfo()
-
-	var stats = getStats()
-	printStats(stats)
+	listenForStats()
 }
 
-func getStats() NodeStats {
-	v, _ := mem.VirtualMemory()
-	var cpus, _ = cpu.Percent(time.Duration(5), true)
-	return NodeStats{
-		memoryTotal: v.Total,
-		memoryFree:  v.Free,
-		cpuPercents: cpus,
-		diskUsage:   getDiskUsage(),
+func listenForStats() {
+	var diskIO = IOAggregator{}
+	var netIO = IOAggregator{}
+
+	for _ = range time.Tick(STATS_POLL_RATE) {
+
+		v, _ := mem.VirtualMemory()
+		var cpus, _ = cpu.Percent(time.Duration(5), true)
+
+		var stats = NodeStats{
+			memory:      v,
+			cpuPercents: cpus,
+			diskUsage:   getDiskUsage(),
+			diskIO:      getDiskIO(&diskIO),
+			netIO:       getNetIO(&netIO),
+		}
+
+		printStats(stats)
 	}
+}
+
+func getDiskIO(diskIO *IOAggregator) *IOStats {
+	var counters, err = disk.IOCounters()
+
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	var readBytes uint64 = 0
+	var writeBytes uint64 = 0
+
+	for _, v := range counters {
+		readBytes += v.ReadBytes
+		writeBytes += v.WriteBytes
+	}
+	var stats = diskIO.UpdateAggregates(readBytes, writeBytes)
+	return &stats
+}
+
+func getNetIO(diskIO *IOAggregator) *IOStats {
+	var counters, err = net.IOCounters(false)
+
+	if err != nil {
+		fmt.Println(err)
+	} else if len(counters) >= 1 {
+		var stats = diskIO.UpdateAggregates(counters[0].BytesRecv, counters[0].BytesSent)
+		return &stats
+	}
+	return nil
 }
 
 func getDiskUsage() []*disk.UsageStat {
@@ -89,13 +131,22 @@ func printSystemInfo() {
 
 func printStats(stats NodeStats) {
 	fmt.Printf("========================= Stats =========================\n")
-	fmt.Printf("Cpu percent:           %f%% %v, cpu(s)\n", avg(stats.cpuPercents), len(stats.cpuPercents))
-	fmt.Printf("Memory used:           %s/%s\n", humanize.Bytes(stats.memoryTotal-stats.memoryFree), humanize.Bytes(stats.memoryTotal))
+	fmt.Printf("Cpu percent:           %f%%, %v cpu(s)\n", avg(stats.cpuPercents), len(stats.cpuPercents))
+	fmt.Printf("Memory used:           %s/%s\n", humanize.Bytes(stats.memory.Used), humanize.Bytes(stats.memory.Total))
 	fmt.Printf("Disk usage:\n")
 	for _, usage := range stats.diskUsage {
 		fmt.Printf("  - %s: %s/%s (%v%%)\n", usage.Path, humanize.Bytes(usage.Used), humanize.Bytes(usage.Total), usage.UsedPercent)
 	}
 
+	if stats.diskIO != nil {
+		fmt.Printf("Disk IO: R:%sps, W:%sps\n", humanize.Bytes(stats.diskIO.readBps), humanize.Bytes(stats.diskIO.writeBps))
+	}
+
+	if stats.netIO != nil {
+		fmt.Printf("NET IO: R:%sps, S:%sps\n", humanize.Bytes(stats.netIO.readBps), humanize.Bytes(stats.netIO.writeBps))
+	}
+	fmt.Println()
+	fmt.Println()
 }
 
 func avg(array []float64) float64 {
