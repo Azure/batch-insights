83d8af59db2908b2c7231e3e3109a9753d1df7d0
Timothee Guerin <tiguerin@microsoft.com>
2018-09-10 10:50:01 -0700

    WIp

--- dev.sh
+++ dev.sh
@@ -1,11 +1,14 @@
 set -e
 
-add-apt-repository ppa:longsleep/golang-backports
 apt-get update  
-apt-get install -y golang-go git
+apt-get install -y git
+
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+gvm install go1.11
+
+echo GO version $(go --version)
 
 git clone https://github.com/Azure/batch-insights -b feature/go
 
-echo GO version $(go --version)
 cd batch-insights
 go run ./..
