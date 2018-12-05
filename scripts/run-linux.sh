2da0cc48f17319c99a84a434f0be63fc76b829b0
Timothee Guerin <tiguerin@microsoft.com>
2018-12-05 14:39:16 -0800

    Publish artifacts

--- scripts/run-linux.sh
+++ scripts/run-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e;
-wget -o ./batch-insights "$BATCH_INSIGHTS_DOWNLOAD_URL";
+wget -O ./batch-insights "$BATCH_INSIGHTS_DOWNLOAD_URL";
 chmod +x ./batch-insights;
 ./batch-insights > node-stats.log &
