ae04414764b976d34944ffab0628566bdc3a5d9a
Timothee Guerin <tiguerin@microsoft.com>
2018-12-10 14:34:06 -0800

    Linux support

---
+++ nvml/nvml_linux.go
@@ -0,0 +1,62 @@
+// +build windows
+
+package nvml
+
+import (
+	nvml_linux "github.com/mindprince/gonvml"
+)
+
+type LinuxNvmlClient struct {
+}
+
+func New() (*LinuxNvmlClient, error) {
+	client := LinuxNvmlClient{}
+
+	return &client, nil
+}
+
+func (client *LinuxNvmlClient) Init() error {
+	return nvml_linux.Init()
+}
+
+func (client *LinuxNvmlClient) Shutdown() error {
+	return nvml_linux.Shutdown()
+}
+
+func (client *LinuxNvmlClient) GetDeviceCount() (uint, error) {
+	value, err := nvml_linux.DeviceCount()
+	if err != nil {
+		return 0, err
+	}
+
+	return uint(value), nil
+}
+
+func (client *LinuxNvmlClient) DeviceGetUtilizationRates(device Device) (GPUUtilization, error) {
+	value, err := nvml_linux.UtilizationRates(nvml_linux.Device(device))
+	if err != nil {
+		return GPUUtilization{GPU: 0, Memory: 0}, err
+	}
+
+	use := GPUUtilization{
+		GPU:    uint(value.GPU),
+		Memory: uint(value.Memory),
+	}
+	return use, nil
+}
+
+func (client *LinuxNvmlClient) DeviceGetMemoryInfo(device Device) (Memory, error) {
+	use, err := nvml_linux.MemoryInfo(nvml_linux.Device(device))
+	if err != nil {
+		return Memory(use), err
+	}
+	return Memory(use), nil
+}
+
+func (client *LinuxNvmlClient) DeviceGetHandleByIndex(index uint) (Device, error) {
+	device, err := nvml_linux.DeviceHandleByIndex(uint32(index))
+	if err != nil {
+		return Device(device), err
+	}
+	return Device(device), nil
+}
