433845d63178177e44518e317ff9b0ae368c6245
Timothee Guerin <tiguerin@microsoft.com>
2018-12-10 15:22:37 -0800

    Linux support

--- scripts/gpu-init.sh
+++ scripts/gpu-init.sh
@@ -1,6 +1,6 @@
 set -e
-apt remove nvidia-cuda-toolkit
-apt remove nvidia-*
+# apt remove nvidia-cuda-toolkit
+# apt remove nvidia-*
 apt update
 apt-key adv --fetch-keys  http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 bash -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
