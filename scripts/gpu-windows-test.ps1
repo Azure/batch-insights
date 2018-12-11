903d83db5385eacefad380aa7ecb657306d58c12
Timothee Guerin <tiguerin@microsoft.com>
2018-12-11 09:05:49 -0800

    Remove unused code

--- scripts/gpu-windows-test.ps1
+++ scripts/gpu-windows-test.ps1
@@ -4,7 +4,7 @@ $wd = $env:AZ_BATCH_TASK_WORKING_DIR
 
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y golang git mingw
-# $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
 git clone https://github.com/Azure/batch-insights -b feature/go-gpu
 
