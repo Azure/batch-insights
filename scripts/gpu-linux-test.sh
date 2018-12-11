9c45b1eafd61b678a29cc93a4ea96bd75f631900
Timothee Guerin <tiguerin@microsoft.com>
2018-12-11 08:54:48 -0800

    update scripts

--- scripts/gpu-linux-test.sh
+++ scripts/gpu-linux-test.sh
@@ -8,3 +8,21 @@ apt update
 apt install -y nvidia-driver-410 --no-install-recommends
 apt install -y cuda-10-0 --no-install-recommends
 apt-get install -y git binutils bison build-essential --no-install-recommends
+
+
+export GOROOT=/usr/local/go
+if [ -d "$GOROOT" ]; then rm -rf $GOROOT; fi
+
+wget https://dl.google.com/go/go1.11.linux-amd64.tar.gz
+tar -xvf go1.11.linux-amd64.tar.gz
+mv go /usr/local
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
+echo GO version $(go version)
+
+git clone https://github.com/Azure/batch-insights -b feature/go-gpu
+
+cd batch-insights
+go build
+
+./batch-insights
