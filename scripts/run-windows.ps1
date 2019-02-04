b019b6d9472585db6d297c66d6066a17cecf3225
Christian <smith.cba@gmail.com>
2019-02-04 12:40:17 +1100

    Update slash per comment from Tim.

--- scripts/run-windows.ps1
+++ scripts/run-windows.ps1
@@ -1,6 +1,6 @@
 $wd = $env:AZ_BATCH_TASK_WORKING_DIR
 
-$exe = "$wd\batch-insights.exe"
+$exe = "$wd/batch-insights.exe"
 
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 Invoke-WebRequest -Uri $env:BATCH_INSIGHTS_DOWNLOAD_URL -OutFile $exe
