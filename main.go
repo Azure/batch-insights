801dce68dd9dc17428eede66090b3817f2a5dae9
Timothee Guerin <tiguerin@microsoft.com>
2019-02-21 16:12:10 -0800

    Build config

--- main.go
+++ main.go
@@ -1,48 +1,83 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/Azure/batch-insights/pkg"
 	"os"
 	"strings"
 )
 
+func parseListArgs(value string) []string {
+	return strings.Split(value, ",")
+}
+
+func getenv(key string) *string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return nil
+	}
+	return &value
+}
+
 func main() {
-	var appInsightsKey = os.Getenv("APP_INSIGHTS_INSTRUMENTATION_KEY")
-	var poolId = os.Getenv("AZ_BATCH_POOL_ID")
-	var nodeId = os.Getenv("AZ_BATCH_NODE_ID")
-	var processNamesStr = os.Getenv("AZ_BATCH_MONITOR_PROCESSES")
+	disableArg := flag.String("disable", "", "List of metrics to disable")
+	processArg := flag.String("process", "", "List of process name to watch")
 
-	if len(os.Args) > 2 {
-		poolId = os.Args[1]
-		nodeId = os.Args[2]
+	envConfig := batchinsights.UserConfig{
+		InstrumentationKey: getenv("APP_INSIGHTS_INSTRUMENTATION_KEY"),
+		PoolID:             getenv("AZ_BATCH_POOL_ID"),
+		NodeID:             getenv("AZ_BATCH_NODE_ID"),
+	}
+	processEnv := getenv("AZ_BATCH_MONITOR_PROCESSES")
+	if processEnv != nil {
+		envConfig.Process = parseListArgs(*processEnv)
+	}
+	argsConfig := batchinsights.UserConfig{
+		PoolID:             flag.String("poolID", "", "Batch pool ID"),
+		NodeID:             flag.String("nodeID", "", "Batch node ID"),
+		Aggregation:        flag.Int("aggregation", 1, "Aggregation in minutes"),
+		InstrumentationKey: flag.String("instkey", "", "Application Insights instrumentation KEY"),
 	}
 
-	if len(os.Args) > 3 {
-		appInsightsKey = os.Args[3]
+	flag.Parse()
+	if processArg != nil {
+		argsConfig.Process = parseListArgs(*processArg)
+	}
+	if disableArg != nil {
+		argsConfig.Disable = parseListArgs(*disableArg)
 	}
 
-	if len(os.Args) > 4 {
-		processNamesStr = os.Args[4]
-	}
+	config := envConfig.Merge(argsConfig)
+	config.Print()
 
-	processNames := strings.Split(processNamesStr, ",")
-	for i := range processNames {
-		processNames[i] = strings.TrimSpace(processNames[i])
-	}
+	fmt.Printf("%v\n", flag.Args())
 
-	batchinsights.PrintSystemInfo()
-	fmt.Printf("   Pool ID: %s\n", poolId)
-	fmt.Printf("   Node ID: %s\n", nodeId)
+	// if len(os.Args) > 3 {
+	// 	appInsightsKey = os.Args[3]
+	// }
 
-	hiddenKey := "-"
-	if appInsightsKey != "" {
-		hiddenKey = "xxxxx"
-	}
+	// if len(os.Args) > 4 {
+	// 	processNamesStr = os.Args[4]
+	// }
 
-	fmt.Printf("   Instrumentation Key: %s\n", hiddenKey)
+	// processNames := strings.Split(processNamesStr, ",")
+	// for i := range processNames {
+	// 	processNames[i] = strings.TrimSpace(processNames[i])
+	// }
 
-	fmt.Printf("   Monitoring processes: %s\n", strings.Join(processNames, ", "))
+	// batchinsights.PrintSystemInfo()
+	// fmt.Printf("   Pool ID: %s\n", poolId)
+	// fmt.Printf("   Node ID: %s\n", nodeId)
 
-	batchinsights.ListenForStats(poolId, nodeId, appInsightsKey, processNames)
+	// hiddenKey := "-"
+	// if appInsightsKey != "" {
+	// 	hiddenKey = "xxxxx"
+	// }
+
+	// fmt.Printf("   Instrumentation Key: %s\n", hiddenKey)
+
+	// fmt.Printf("   Monitoring processes: %s\n", strings.Join(processNames, ", "))
+
+	// batchinsights.ListenForStats(poolId, nodeId, appInsightsKey, processNames)
 }
--- pkg/config.go
+++ pkg/config.go
@@ -1,18 +1,58 @@
 package batchinsights
 
 import (
+	"fmt"
 	"strings"
 	"time"
 )
 
 // UserConfig config provided by the user either via command line, file or environemnt variable.
 type UserConfig struct {
-	PoolID             string
-	NodeID             string
+	PoolID             *string
+	NodeID             *string
 	InstrumentationKey *string  // Application insights instrumentation key
 	Process            []string // List of process names to watch
 	Aggregation        *int     // Local aggregation of data in minutes (default: 1)
 	Disable            []string // List of metrics to disable
+}
+
+// Print print the config to console
+func (config UserConfig) Print() {
+	fmt.Printf("User configuration:\n")
+	fmt.Printf("   Pool ID: %s\n", *config.PoolID)
+	fmt.Printf("   Node ID: %s\n", *config.NodeID)
+	hiddenKey := "-"
+	if config.InstrumentationKey != nil {
+		hiddenKey = "xxxxx"
+	}
+
+	fmt.Printf("   Instrumentation Key: %s\n", hiddenKey)
+	fmt.Printf("   Aggregation: %d\n", *config.Aggregation)
+	fmt.Printf("   Disable: %v\n", config.Disable)
+	fmt.Printf("   Process: %v\n", config.Process)
+}
+
+// Merge with another config
+func (config UserConfig) Merge(other UserConfig) UserConfig {
+	if other.PoolID != nil {
+		config.PoolID = other.PoolID
+	}
+	if other.NodeID != nil {
+		config.NodeID = other.NodeID
+	}
+	if other.InstrumentationKey != nil {
+		config.InstrumentationKey = other.InstrumentationKey
+	}
+	if other.Aggregation != nil {
+		config.Aggregation = other.Aggregation
+	}
+	if len(other.Process) > 0 {
+		config.Process = other.Process
+	}
+	if len(other.Disable) > 0 {
+		config.Disable = other.Disable
+	}
+	return config
 }
 
 // DisableConfig config showing which feature are disabled
@@ -27,8 +67,8 @@ type DisableConfig struct {
 
 // Config General config batch insights takes as input
 type Config struct {
-	PoolID             string
-	NodeID             string
+	PoolID             *string
+	NodeID             *string
 	InstrumentationKey *string
 	Process            []string
 	Aggregation        time.Duration
