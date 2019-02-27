d28b065a665a3165c0406dd2565f7695ad4b4a7a
Timothee Guerin <tiguerin@microsoft.com>
2019-02-27 14:14:45 -0800

    Wip

--- scripts/dev-windows.ps1
+++ scripts/dev-windows.ps1
@@ -29,7 +29,7 @@ if($exists)
 $toolArgs =  "--poolID $env:AZ_BATCH_POOL_ID --nodeID $env:AZ_BATCH_NODE_ID --instKey $env:APP_INSIGHTS_INSTRUMENTATION_KEY $AZ_BATCH_INSIGHTS_ARGS"
 
 Write-Host "Starting App insights background process in $wd"
-$action = New-ScheduledTaskAction -WorkingDirectory $wd -Execute 'Powershell.exe' -Argument "Start-Process $exe -Argument $toolArgs -RedirectStandardOutput .\batch-insights.log -RedirectStandardError .\batch-insights.err.log -NoNewWindow"  
+$action = New-ScheduledTaskAction -WorkingDirectory $wd -Execute 'cmd.exe' -Argument "/c $exe  $toolArgs > .\batch-insights.log 2>&1"  
 $principal = New-ScheduledTaskPrincipal -UserID 'NT AUTHORITY\SYSTEM' -LogonType ServiceAccount -RunLevel Highest ; 
 Register-ScheduledTask -Action $action -Principal $principal -TaskName "batchappinsights" -Force ; 
 Start-ScheduledTask -TaskName "batchappinsights"; 
